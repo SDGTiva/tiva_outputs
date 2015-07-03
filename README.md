3d3575c650b9125ba9d6aafe80ef4c638f2fef96
Ângelo Nuffer <angelonuffer@gmail.com>
2015-07-03 21:01:55 +0000

    Release version 0.1.

---
+++ README.md
@@ -0,0 +1,12 @@
+Tiva Outputs v0.1.0
+===================
+
+0. OBJECTIVE
+
+	0. Register goods and services of organization.
+
+0. INSTALLING
+
+	0. Install using python-setuptools.
+
+		>  # python setup.py install
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "tiva outputs",
-    version = "0.0.0",
+    version = "0.1.0",
     author = "Ângelo Nuffer",
     author_email = "angelonuffer@gmail.com",
     packages = find_packages(),
